7d70eee6e70b329a81e1dae97b63778b494f4440
temur <temur@MBP-von-Sayna.fritz.box>
2023-02-05 17:45:14 +0100

    added mail

--- driver_alert-2.py
+++ driver_alert-2.py
@@ -2,10 +2,34 @@ import mysql.connector
 import logging as log
 from twilio.rest import Client
 from time import sleep
+import smtplib
+
 
 log.basicConfig(filename='log.log', level=log.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
 frist_run = True
+
+# Twilio credentials
+tw_account_sid = "AC5d5567ee7f64c99283cd6dc5fb9c7fe0"
+tw_auth_token = "d72ffeec8bd614ec17942e826ea57876"
+tw_url = "http://demo.twilio.com/docs/voice.xml"
+tw_to = "+4917663385873"
+tw_from_ = "+19135132511"
+
+# Database credentials
+db_host = "db-web9.alfahosting-server.de"
+db_user = "up6hge6e_driver"
+db_password = "HJScs@!16031955"
+db_name = "up6hge6e_jom"
+
+# Email credentials
+e_sender_email = "monitor_driverdb@outlook.de"
+e_receiver_email = "msok.it@icloud.com"
+e_password = "This!as2well"
+e_message_done = """\
+Subject: Bot is done
+
+Bot completed its task and found new rows in the database."""
 
 class alert_bot():
     log.info("Starting")
@@ -15,19 +39,17 @@ class alert_bot():
             print("Connecting to database")
             log.info("Connecting to database") 
             self.db = mysql.connector.connect(
-                host="db-web9.alfahosting-server.de",
-                user="up6hge6e_driver",
-                password="HJScs@!16031955",
-		        database="up6hge6e_jom")
+                host=db_host,
+                user=db_user,
+                password=db_password,
+		        database=db_name)
         except Exception as e:
             print(e)
             log.error("Could not connect to database")
         try:
             print("Creating Twilio client")
             log.info("Creating Twilio client")
-            account_sid = "AC5d5567ee7f64c99283cd6dc5fb9c7fe0"
-            auth_token = "d72ffeec8bd614ec17942e826ea57876"
-            self.client = Client(account_sid, auth_token)
+            self.client = Client(tw_account_sid, tw_auth_token)
         except Exception as e:
             log.error(e)
             log.error("Could not create Twilio client")
@@ -68,6 +90,7 @@ class alert_bot():
                 print("New row found")
                 log.info("New row found")
                 self.call_me()
+                self.send_email(e_message_done)
             else:
                 log.info("Rows already in cache")
                 print("Rows already in cache")
@@ -91,13 +114,28 @@ class alert_bot():
                 print("Calling user")
                 log.info("Calling user")
                 call = self.client.calls.create(
-  			url="http://demo.twilio.com/docs/voice.xml",
-  			to="+4917663385873",
-  			from_="+19135132511"
-			)
+                    url="http://demo.twilio.com/docs/voice.xml",
+                    to="+4917663385873",
+                    from_="+19135132511"
+                    )
             except Exception as e:
                 log.error(e)
                 log.error("Could not call user")
+
+    def send_email(msg, self):
+        try:
+            print("Sending email")
+            log.info("Sending email")
+            server = smtplib.SMTP('smtp-mail.outlook.com', 587)
+            server.starttls()
+            server.login(e_sender_email, e_password)
+            server.sendmail(e_sender_email, e_receiver_email, msg)
+            server.quit()
+            log.info("Email sent")
+            print("Email sent")
+        except Exception as e:
+            log.error(e)
+            log.error("Could not send email")
 
     def get_all_uniq_id(self):
         log.info("Getting all uniq_id")
@@ -125,4 +163,4 @@ if __name__ == "__main__":
     bot = alert_bot()
     while True:
         bot.check_db()
-        sleep(600)
+        sleep(5)
