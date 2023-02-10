9f15b5f72e0db06141938689ea0f1e93db47ccf1
Ti Mo <mosk.it@icloud.com>
2023-02-10 11:56:42 +0100

    added example config file(einst)

---
+++ new_version/einst.py
@@ -0,0 +1,17 @@
+# Twilio credentials
+tw_account_sid = "AC5d5567ee7f64c99283cd6dc5fb9c7fe0"
+tw_auth_token = "694855d3520240d12ac499d8959ccad2"
+tw_url = "http://demo.twilio.com/docs/voice.xml"
+tw_to = "+4917663385873"
+tw_from = "+19135132511"
+
+# Database credentials
+db_host = "db-web9.alfahosting-server.de"
+db_user = "up6hge6e_driver"
+db_password = "HJScs@!16031955"
+db_name = "up6hge6e_jom"
+
+# Email credentials
+e_sender_email = "monitor_driverdb@outlook.de"
+e_receiver_email = "mosk.it@icloud.com"
+e_password = "This!as2well"
