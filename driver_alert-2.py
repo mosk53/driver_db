abd9948bdf3c253805c090d2c3b64652d589c9a3
Temur <89967825+mosk53@users.noreply.github.com>
2023-02-10 11:50:13 +0100

    Update driver_alert-2.py

--- driver_alert-2.py
+++ driver_alert-2.py
@@ -5,29 +5,10 @@ from time import sleep
 import smtplib
 from pythonping import ping
 import pickle
+from einst import *
 
 
 log.basicConfig(filename='log.log', level=log.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-
-# Twilio credentials
-tw_account_sid = "AC5d5567ee7f64c99283cd6dc5fb9c7fe0"
-tw_auth_token = "694855d3520240d12ac499d8959ccad2"
-tw_url = "http://demo.twilio.com/docs/voice.xml"
-tw_to = "+4917663385873"
-tw_from_ = "+19135132511"
-
-# Database credentials
-db_host = "db-web9.alfahosting-server.de"
-db_user = "up6hge6e_driver"
-db_password = "HJScs@!16031955"
-db_name = "up6hge6e_jom"
-
-# Email credentials
-e_sender_email = "monitor_driverdb@outlook.de"
-e_receiver_email = "mosk.it@icloud.com"
-e_password = "This!as2well"
-e_message_done = """\
-Subject: Bot is done
 
 Bot completed its task and found new rows in the database."""
 
